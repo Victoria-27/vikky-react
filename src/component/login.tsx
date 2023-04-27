4bdb0dd3ec8ad18594e349e5a7cbb6bce58c749e
Samson Ajulor <samsonajulor@gmail.com>
2023-04-28 00:16:57 +0100

    added form validations

--- src/component/login.tsx
+++ src/component/login.tsx
@@ -14,12 +14,27 @@ const LoginContainer = styled.div`
   font-weight: bold;
 `;
 
+const ErrorText = styled.p`
+  color: red;
+  font-size: 14px;
+`;
+
 const Login: React.FC<LoginProps> = ({ onLogin }) => {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
+  const [usernameError, setUsernameError] = useState(false);
+  const [passwordError, setPasswordError] = useState(false);
 
   const handleLogin = () => {
-    onLogin(username, password);
+    if (!username) {
+      setUsernameError(true);
+    }
+    if (password.length < 6) {
+      setPasswordError(true);
+    }
+    if (username && password.length >= 6) {
+      onLogin(username, password);
+    }
   };
 
   return (
@@ -27,11 +42,31 @@ const Login: React.FC<LoginProps> = ({ onLogin }) => {
       <h2>Login</h2>
       <div>
         <label htmlFor='username'>Username:</label>
-        <input type='text' id='username' value={username} onChange={(e) => setUsername(e.target.value)} />
+        <input
+          type='text'
+          id='username'
+          value={username}
+          onChange={(e) => {
+            setUsername(e.target.value);
+            setUsernameError(false);
+          }}
+          style={{ border: usernameError ? '1px solid red' : '1px solid black' }}
+        />
+        {usernameError && <ErrorText>Please enter a username</ErrorText>}
       </div>
       <div>
         <label htmlFor='password'>Password:</label>
-        <input type='password' id='password' value={password} onChange={(e) => setPassword(e.target.value)} />
+        <input
+          type='password'
+          id='password'
+          value={password}
+          onChange={(e) => {
+            setPassword(e.target.value);
+            setPasswordError(false);
+          }}
+          style={{ border: passwordError ? '1px solid red' : '1px solid black' }}
+        />
+        {passwordError && <ErrorText>Password must be at least 6 characters long</ErrorText>}
       </div>
       <button onClick={handleLogin}>Login</button>
     </LoginContainer>
